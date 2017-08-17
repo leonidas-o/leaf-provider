fc7376bcfc6469f9eeb4ce495177e5031021ee9a
Leonid Orsulic <lorsulic@Leonids-MBP16.fritz.box>
2017-08-17 11:32:24 +0200

    adds swift4 header and syntax, updates dependencies on vapor 2.2.1 and leaf beta branch

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "LeafProvider",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/leaf.git", majorVersion: 2),
+        .package(url: "https://github.com/vapor/vapor.git", from: "2.2.1"),
+        .package(url: "https://github.com/vapor/leaf.git", branch("beta"),
     ]
 )
